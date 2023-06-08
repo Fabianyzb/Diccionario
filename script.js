c6b1f5d295b159695d2cc23006b54386d168187a
Fabian Yanez <fabian.yzb@gmail.com>
2023-06-08 01:27:09 -0400

    cambios variables sinonimos malos

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const wrapper = document.querySelector('.wrapper'),
-searchInput = wrapper.querySelector('input');
+searchInput = wrapper.querySelector('input'),
+synonyms = wrapper.querySelector('.synonyms .list'),
 infoText = wrapper.querySelector('.info-text');
 
 //data function
@@ -8,17 +9,30 @@ function data(result, word){
         infoText.innerHTML = `Can't find de meaning of <span>'${word}'</span>. Please, try to search for another word.`;
     } else {
         console.log(result);
-        wrapper.classList.add('active');
-        let definitions = result[0].meanings[0].definitions[0,1],
-        phonetics = `${result[0].meanings[0].partOfSpeech} /${result[0].phonetics[0].text}/`;
+        wrapper.classList.add('active'); // If searched word exist then active class will be added on the wrapper;
+
+
+        let definitions = result[0].meanings[0].definitions[0];
+        let synonym = result[0].meanings[0].synonyms
+        let examples = result[0].meanings[0].definitions[0].example
+        phonetics = `${result[0].meanings[0].partOfSpeech} /${result[0].phonetics[2].text}`;
 
         //passing the particular response data to a particular html element
         document.querySelector('.word p').innerText = result[0].word;
-        document.querySelector('.word span').innerText = result[0].word;
-        document.querySelector('.meaning span').innerText = definitions.definitions;
-        document.querySelector('.example span').innerText = definitions.example;   
-   
-   
+        document.querySelector('.word span').innerText = phonetics;
+        document.querySelector('.meaning span').innerText = definitions.definition;
+        document.querySelector('.example span').innerText = examples; 
+        if (synonym[0] == undefined){
+            synonyms.parentElement.style.display = 'none';
+            
+        } else {
+            synonyms.parentElement.style.display = 'block';
+            synonyms.innerHTML = '';
+            for (let i = 0; i < 5; i++) { //geting only 5 synonyms out of many 
+                let tag = `<span>${synonym[i]},</span>`;
+                synonyms.insertAdjacentHTML('beforeend', tag); //passing all 5 synonyms inside synonyms div 
+            }
+        }  
     }
 }
 
